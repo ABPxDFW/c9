429f83f0dd960bcaeec23ba65d40f2d398e899b6
Bobby <abpupeerapitug@yahoo.com>
2017-08-08 03:45:29 +0000

    Modified app.js

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -22,3 +22,17 @@ app.get("/dog", function(req, res){
     console.log("Someone made a request to /dog.")
     res.send("WOOF!");
 });
+
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!")
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+    res.send("WELCOME TO THE COMMENTS PAGE!");
+});
+
+app.get("*", function(req, res){
+    res.send("YOU ARE A STAR!!!");
+});
+
